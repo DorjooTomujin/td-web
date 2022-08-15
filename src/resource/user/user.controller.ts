5a30b54ebca222566bb83096e60bda08470c4bae
Shaazgai <bzao.hover@gmail.com>
2022-08-15 14:09:00 +0800

    test

--- src/resource/user/user.controller.ts
+++ src/resource/user/user.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from "@nestjs/common";
+import { ApiOperation, ApiTags } from "@nestjs/swagger";
+import { UserDto } from "./dto/user.dto";
 import { UserService } from "./user.service";
 
+@ApiTags('user')
 @Controller('user')
 export class UserController {
     constructor(private service: UserService) { }
     @Get('views')
+    @ApiOperation({})
     fetchAllUser() {
         return this.service.fetchAllUsers()
     }
